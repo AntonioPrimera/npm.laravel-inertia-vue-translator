dd7ac4729d24bd1d3600dcab8c3466e779cb6fbb
Antonio Primera <antonio@cus.ro>
2024-04-27 17:33:25 +0300

    Updated the export name for the plugin from inertiaPlugin to translatorPlugin for better readability inside the app creation script

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import Translator from "./translator";
-import inertiaPlugin from "./vue3InertiaPlugin";
+import translatorPlugin from "./vue3InertiaPlugin";
 import injectionKey from "./translatorInjectionKey";
 import {inject} from "vue";
 
@@ -9,5 +9,5 @@ const useTranslator = () => inject(injectionKey);
 export {
 	Translator,			//the translator class (so it can be extended or instantiated)
 	useTranslator,		//the injection method (to access the translator methods in the component scripts)
-	inertiaPlugin,		//the Inertia plugin (to add the translator methods to the app)
+	translatorPlugin,		//the Inertia plugin (to add the translator methods to the app)
 };
